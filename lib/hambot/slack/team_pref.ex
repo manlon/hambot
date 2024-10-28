1d9f0fd025318204032c3c584be2bbda45019ff4
Matt Hanlon <matthanlon@gmail.com>
2024-10-27 22:32:49 -0700

    default reply mode is :thread

--- lib/hambot/slack/team_pref.ex
+++ lib/hambot/slack/team_pref.ex
@@ -9,7 +9,7 @@ defmodule Hambot.Slack.TeamPref do
 
     @primary_key false
     embedded_schema do
-      field :archive_link_mode, Ecto.Enum, values: [:reply, :thread, :none], default: :reply
+      field :archive_link_mode, Ecto.Enum, values: [:reply, :thread, :none], default: :thread
     end
 
     def changeset(prefs, attrs) do
